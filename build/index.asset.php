799f935cfc15f4e56180721cc57a80c537a7f987
CoderAbhinav <abhinavrb.a02@gmail.com>
2024-07-08 19:18:39 +0530

    Initial Commit
    Description:
    Created a block basic structure using `npx @wordpress/create-block` command

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f5dfd4d57ce9c52eeafe');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '64554f0a7a16787f7756');
